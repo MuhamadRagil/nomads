59956fc31bdca8e68ae1d71bca052dec9ffda581
MuhamadRagil <muhammadragil1403@gmail.com>
2022-07-22 17:16:30 +0700

    add nomads folder to git

---
+++ resources/views/includes/navbar-alternate.blade.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <nav class="row navbar navbar-expand-lg navbar-light bg-white">
+
+        <a href="{{ route('home') }}" class="navbar-brand navbar-nav ml-auto mr-auto mr-sm-auto mr-lg-0 mr-md-auto">
+            <img src="{{ url('frontend/images/logo.png') }}" alt="logo NOMADS" srcset="">
+        </a>
+        <ul class="navbar-nav mr-auto d-none d-lg-block">
+            <li>
+                <span class="text-muted">
+                    | &nbsp; Beyond the explorer of the world
+                </span>
+            </li>
+        </ul>
+    </nav>
+</div>
