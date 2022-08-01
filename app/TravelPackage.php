5722c391b2917ba200a87a01348ff466bddabe63
MuhamadRagil <muhammadragil1403@gmail.com>
2022-08-01 13:53:03 +0700

    add new feature transaction

--- app/TravelPackage.php
+++ app/TravelPackage.php
@@ -17,6 +17,10 @@ class TravelPackage extends Model
 
     protected $hidden = [
 
-    ]; 
+    ];
+    
+    public function galleries(){
+        return $this->hasMany(Gallery::class, 'travel_packages_id', 'id');
+    }
     
 }
